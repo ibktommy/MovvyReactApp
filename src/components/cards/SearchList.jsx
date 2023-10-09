af3dc16966e2b312181c7e76909f9d3055945569
ibktommy <atomquartz@gmail.com>
2023-10-09 20:13:14 +0100

    Add the searchTerm as a condition-value which the useQuery uses to determin if it will perform a new fetch if the searchTerm has changed

--- src/components/cards/SearchList.jsx
+++ src/components/cards/SearchList.jsx
@@ -4,20 +4,21 @@ import SearchListStyles from "../../assets/styles/SearchListStyles";
 import { RingLoader } from 'react-spinners';
 import SearchItem from '../cards/SearchItem'
 
+const apikey = import.meta.env.VITE_API_ACCESS_KEY;
 
 const SearchList = ({ searchTerm, category }) => {
 	let searchURL = '';
 
 	if (searchTerm && category === 'movies') {
-		searchURL = `https://api.themoviedb.org/3/search/movie?query=${searchTerm}&api_key=30b4ef3f8b7fd7bcf7870dddc7c9a135`;
+		searchURL = `https://api.themoviedb.org/3/search/movie?query=${searchTerm}&api_key=${apikey}`;
 	} else if (searchTerm && category === 'TV series') {
-		searchURL = `https://api.themoviedb.org/3/search/tv?query=${searchTerm}&include_adult=false&language=en-US&page=1',`;
+		searchURL = `https://api.themoviedb.org/3/search/tv?query=${searchTerm}&include_adult=false&language=en-US&api_key=${apikey}&page=1',`;
 	}
 
 	console.log(searchURL);
 
   const fetchSearchTerm = useQuery({
-		queryKey: ['searchResult'],
+		queryKey: ['searchResult', searchTerm],
 		queryFn: async () => {
 			const result = await axios.get(searchURL);
 
@@ -70,7 +71,7 @@ const SearchList = ({ searchTerm, category }) => {
 	return (
     <SearchListStyles>
       <h3>Search results:</h3>
-      <SearchItem searchResult={data}/>
+      <SearchItem searchResult={data.data.results}/>
     </SearchListStyles>
   );
 };
